01653d46cde760be9607c1fc56074d28725ef181
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-27 21:14:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,12 @@
-class Shoe 
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
   
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble 
+    puts "The shoe has been repaired"
+    @condition = new 
+  end
 end
